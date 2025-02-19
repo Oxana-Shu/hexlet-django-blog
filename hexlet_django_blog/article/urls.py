18665ffbc7d730c992316ffab85078ecdd7fe24a
Oxana Shu <laskka9@gmail.com>
2025-02-19 17:52:51 +0300

    routing

--- hexlet_django_blog/article/urls.py
+++ hexlet_django_blog/article/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 from hexlet_django_blog.article import views
 
 urlpatterns = [
+    path('<str:tags>/<int:article_id>/', views.index, name='article'),
     path('', views.IndexView.as_view()),
+    
 ]
--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,8 +1,16 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.views import View
+from django.shortcuts import redirect
+from django.urls import reverse
 
 class IndexView(View):
 
-    def get(self, request, *args, **kwargs):
-        return HttpResponse('Hello, article!')
+    def get(self, request):
+        return redirect(reverse('article', kwargs={'tags' : 'python', 'article_id' : '42'}))
+    
+def index(request, tags=None, article_id=None):
+    if tags == None:
+        article_id = 42
+        tags = 'python'
+    return render(request, 'articles/index.html', context={'article_id': article_id, 'tags': tags})
