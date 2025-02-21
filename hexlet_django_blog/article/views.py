d672da0c0b9d6fea9f655e3d3ee75c5b0291ca47
Oxana Shu <laskka9@gmail.com>
2025-02-21 12:03:36 +0300

    index

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -4,10 +4,17 @@ from django.views import View
 from django.shortcuts import redirect
 from django.urls import reverse
 
+from hexlet_django_blog.article.models import Article
+
 class IndexView(View):
 
-    def get(self, request):
-        return redirect(reverse('article', kwargs={'tags' : 'python', 'article_id' : '42'}))
+    def get(self, request, *args, **kwargs):
+        articles = Article.objects.all()[:15]
+        return render(request, 'articles/index.html', context={
+            'articles': articles,
+        })
+#    def get(self, request):
+#       return redirect(reverse('article', kwargs={'tags' : 'python', 'article_id' : '42'}))
     
 def index(request, tags=None, article_id=None):
     if tags == None:
