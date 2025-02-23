bfcbe5ff5a3d28d8bee73ddb60f29f4fbdc70dd2
Oxana Shu <laskka9@gmail.com>
2025-02-23 08:47:13 +0300

    update

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -53,7 +53,32 @@ class ArticleFormCreateView(View):
         form = ArticleForm(request.POST)
         if form.is_valid(): # Если данные корректные, то сохраняем данные формы
             form.save()
+            messages.get_messages(request)
+            for message in messages:
+                pass
             return redirect('articles') # Редирект на указанный маршрут
         # Если данные некорректные, то возвращаем человека обратно на страницу с заполненной формой
         messages.error(request, "The article was not created.")
         return render(request, 'articles/create.html', {'form': form, 'messages': messages})
+    
+
+class ArticleFormEditView(View):
+
+    def get(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(instance=article)
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id})
+    
+    def post(self, request, *args, **kwargs):
+        article_id = kwargs.get('id')
+        article = Article.objects.get(id=article_id)
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            form.save()
+            messages.get_messages(request)
+            for message in messages:
+                pass
+            return redirect('articles')
+
+        return render(request, 'articles/update.html', {'form': form, 'article_id':article_id, 'messages': messages})
